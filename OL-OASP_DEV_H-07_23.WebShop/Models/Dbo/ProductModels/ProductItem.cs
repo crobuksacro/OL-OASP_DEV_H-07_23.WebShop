5db6b0c5297ddccab2d46a19f3cb9aadb275d1ad
Ivan Radoš <irados@autozubak.hr>
2024-05-27 20:45:13 +0200

    moj updtte

---
+++ OL-OASP_DEV_H-07_23.WebShop/Models/Dbo/ProductModels/ProductItem.cs
@@ -0,0 +1,15 @@
+﻿using OL_OASP_DEV_H_07_23.WebShop.Shared.Interfaces;
+using OL_OASP_DEV_H_07_23.WebShop.Shared.Models.Base.ProductModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace OL_OASP_DEV_H_07_23.WebShop.Models.Dbo.ProductModels
+{
+    public class ProductItem: ProductItemBase, IBaseTableAtributes
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public bool Valid { get; set; }
+    }
+}
