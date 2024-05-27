c9e2c0c0f3eccf0fbef4d7293c0f9144c8e6aa0a
Ivan Radoš <irados@autozubak.hr>
2024-05-27 20:27:15 +0200

    added product base models

---
+++ OL-OASP_DEV_H-07_23.WebShop.Shared/Models/Base/ProductModels/ProductCategoryBase.cs
@@ -0,0 +1,8 @@
+﻿namespace OL_OASP_DEV_H_07_23.WebShop.Shared.Models.Base.ProductModels
+{
+    public abstract class ProductCategoryBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ OL-OASP_DEV_H-07_23.WebShop.Shared/Models/Base/ProductModels/ProductItemBase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OL_OASP_DEV_H_07_23.WebShop.Shared.Models.Base.ProductModels
+{
+    public abstract class ProductItemBase
+    {
+        [Required]
+        [StringLength(200, MinimumLength = 10)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal Quantity { get; set; }
+
+    }
+}
