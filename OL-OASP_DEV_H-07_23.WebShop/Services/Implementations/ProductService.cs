95bb24ff6b5f92997b39df0f6b0362581feb0f20
Ivan Radoš <irados@autozubak.hr>
2024-05-29 18:39:00 +0200

    update

---
+++ OL-OASP_DEV_H-07_23.WebShop/Services/Implementations/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using OL_OASP_DEV_H_07_23.WebShop.Data;
+
+namespace OL_OASP_DEV_H_07_23.WebShop.Services.Implementations
+{
+    public class ProductService
+    {
+
+        private readonly ApplicationDbContext db;
+        //private readonly IMapper mapper;
+
+        //CRUD: ProductCategory,
+        //CRUD: ProductItem
+
+    }
+}
