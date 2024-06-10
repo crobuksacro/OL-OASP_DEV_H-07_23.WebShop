51ecf0c29d9a3e3a0c7290a73446fd4c166a6f18
Ivan Radoš <irados@autozubak.hr>
2024-06-10 19:36:56 +0200

    update

---
+++ OL-OASP_DEV_H-07_23.WebShop.UnitTest/BuyerServiceUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using OL_OASP_DEV_H_07_23.WebShop.Services.Implementations;
+
+namespace OL_OASP_DEV_H_07_23.WebShop.UnitTest
+{
+    public class BuyerServiceUnitTest : WebShopSetup
+    {
+        private readonly IBuyerService buyerService;
+
+        public BuyerServiceUnitTest()
+        {
+            this.buyerService = GetBuyerService();
+        }
+
+
+
+
+        [Fact]
+        public async void GetOrder_FetchesOrderFromDb_ValidatesIfItemIsNotNull()
+        {
+            var result = await buyerService.GetOrder(Orders[0].Id);
+            Assert.NotNull(result);
+        }
+    }
+}
--- OL-OASP_DEV_H-07_23.WebShop.UnitTest/WebShopSetup.cs
+++ OL-OASP_DEV_H-07_23.WebShop.UnitTest/WebShopSetup.cs
@@ -1,11 +1,15 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Options;
 using Moq;
 using OL_OASP_DEV_H_07_23.WebShop.Data;
 using OL_OASP_DEV_H_07_23.WebShop.Mapping;
+using OL_OASP_DEV_H_07_23.WebShop.Models.Dbo.Common;
+using OL_OASP_DEV_H_07_23.WebShop.Models.Dbo.OrderModels;
 using OL_OASP_DEV_H_07_23.WebShop.Models.Dbo.ProductModels;
+using OL_OASP_DEV_H_07_23.WebShop.Models.Dbo.UserModel;
 using OL_OASP_DEV_H_07_23.WebShop.Services.Implementations;
 using OL_OASP_DEV_H_07_23.WebShop.Services.Interfaces;
 using OL_OASP_DEV_H_07_23.WebShop.Shared.Models.Dto;
@@ -19,10 +23,18 @@ namespace OL_OASP_DEV_H_07_23.WebShop.UnitTest
         protected readonly Mock<IOptions<AppSettings>> AppSettings;
         protected static string TestString = "mali medo";
         protected readonly List<ProductCategory> ProductCategories;
+        protected readonly List<Order> Orders;
+        protected readonly ApplicationUser ApplicationUser;
+        protected readonly Mock<UserManager<ApplicationUser>> UserManager;
+
 
         public WebShopSetup()
         {
             SetupInMemoryContext();
+
+            var userStoreMock = Mock.Of<IUserStore<ApplicationUser>>();
+            UserManager = new Mock<UserManager<ApplicationUser>>(userStoreMock, null, null, null, null, null, null, null, null);
+            ApplicationUser = GetApplicationUser();
             var mappingConfig = new MapperConfiguration(mc =>
             {
                 mc.AddProfile(new MappingProfile());
@@ -35,8 +47,79 @@ namespace OL_OASP_DEV_H_07_23.WebShop.UnitTest
             AppSettings = new Mock<IOptions<AppSettings>>();
             AppSettings.Setup(c => c.Value).Returns(config);
             ProductCategories = GenerateProductCategorys(100);
+            Orders = GetOrders(1);
 
         }
+
+
+        protected ApplicationUser GetApplicationUser()
+        {
+            var applicationUser = new ApplicationUser
+            {
+                UserName = "test",
+                Email = "test@test.hr",
+                Address = new Address
+                {
+                    City = "Zagreb",
+                    Country = "Croatia",
+                    Street = "Ilica 1",
+                    Number = "10a"
+                },
+                LastName = "test",
+                FirstName = "test",
+                PhoneNumber = "123456789",
+                EmailConfirmed = true
+
+            };
+
+            InMemoryDbContext.Users.Add(applicationUser);
+            InMemoryDbContext.SaveChanges();
+            return applicationUser;
+        }
+
+        protected List<Order> GetOrders(int number)
+        {
+            List<Order> orders = new List<Order>();
+
+            for (int i = 0; i < number; i++)
+            {
+                var order = new Order
+                {
+                    Buyer = ApplicationUser,
+                    BuyerId = ApplicationUser.Id,
+                    OrderAddress = new Address
+                    {
+                        City = "Zagreb",
+                        Country = "Croatia",
+                        Street = "Ilica 1",
+                        Number = "10a"
+                    },
+                    Message = TestString,
+                    OrderItems = new List<OrderItem>
+                    {
+                        new OrderItem
+                        {
+                            ProductItem = ProductCategories[0].ProductItems.First(),
+                            Quantity = 10,
+                            Price = 100,
+                           
+                        }
+                    }
+
+                };
+
+                InMemoryDbContext.Orders.Add(order);
+                InMemoryDbContext.SaveChanges();
+                orders.Add(order);
+            }
+
+
+            return orders;
+        }
+
+
+
+
         protected List<ProductCategory> GenerateProductCategorys(int number)
         {
 
@@ -99,6 +182,17 @@ namespace OL_OASP_DEV_H_07_23.WebShop.UnitTest
                 return new ProductService(db, Mapper, AppSettings.Object);
             }
             return new ProductService(InMemoryDbContext, Mapper, AppSettings.Object);
+
+        }
+
+
+        protected IBuyerService GetBuyerService(ApplicationDbContext? db = null)
+        {
+            if (db != null)
+            {
+                return new BuyerService(UserManager.Object, db, Mapper);
+            }
+            return new BuyerService(UserManager.Object, InMemoryDbContext, Mapper);
 
         }
 
